146352a7c94d2229197a1c3bf709d0c548d3c829
Akimosabe <he7seven@gmail.com>
2023-10-01 19:25:14 +0500

    final lr2

--- src/main/java/ru/example2/SecondTestAppSpringBoot/Controller/MyController.java
+++ src/main/java/ru/example2/SecondTestAppSpringBoot/Controller/MyController.java
@@ -8,12 +8,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.example2.SecondTestAppSpringBoot.Exception.UnsupportedCodeException;
 import ru.example2.SecondTestAppSpringBoot.Exception.ValidationFailedException;
 import ru.example2.SecondTestAppSpringBoot.Model.Request;
 import ru.example2.SecondTestAppSpringBoot.Model.Response;
 import ru.example2.SecondTestAppSpringBoot.Service.ValidationService;
 
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 
@@ -29,20 +32,33 @@ public class MyController {
     public ResponseEntity<Response> feedback(@Valid @RequestBody Request request,
                                              BindingResult bindingResult) {
 
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+
+
         Response response = Response.builder()
                 .uid(request.getUid())
                 .operationUid(request.getOperationUid())
-                .systemTime(request.getSystemTime())
+                .systemTime(simpleDateFormat.format(new Date()))
                 .code("success")
                 .errorCode("")
                 .errorMessage("")
                 .build();
         try {
             validationService.isValid(bindingResult);
+
+            if (request.getUid().equals("123")) {
+                throw new UnsupportedCodeException();
+            }
+
         } catch (ValidationFailedException e) {
             response.setCode("Failed");
             response.setErrorCode("ValidationException");
             response.setErrorMessage("Ошибка валидации");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        } catch (UnsupportedCodeException e) {
+            response.setCode("Failed");
+            response.setErrorCode("UnsupportedCodeException");
+            response.setErrorMessage("Запрещенное значение UID");
             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             response.setCode("Failed");
